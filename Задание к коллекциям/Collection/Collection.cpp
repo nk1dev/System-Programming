8dfe5e96cb971cc35712f9f261ff6c166a71438d
nk1dev <76609654+nk1dev@users.noreply.github.com>
2021-06-29 11:04:22 +0300

    up

---
+++ Задание к коллекциям/Collection/Collection.cpp
@@ -0,0 +1,254 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+struct PeoplDt
+{
+    string Name;
+    string Surname;
+    int Age;
+    PeoplDt()
+    {
+        Name = "";
+        Surname = "";
+        Age = 0;
+    };
+    PeoplDt(string Name, string Surname, int Age)
+    {
+        this->Name = Name;
+        this->Surname = Surname;
+        this->Age = Age;
+    }
+    void Print()
+    {
+        cout << "\n";
+        cout << " " << Name << " " << Surname << " " << Age << endl;
+        cout << "\n";
+    }
+};
+struct Element
+{
+    PeoplDt PData;
+    Element* next;
+    Element* pre;
+    Element() { pre = next = nullptr; };
+
+    Element(PeoplDt peopleData)
+    {
+        this->PData = peopleData;
+        pre = next = nullptr;
+    }
+};
+struct ListPeoples
+{
+
+    ListPeoples() { First = Last = nullptr; SizeList = 0; }
+    ~ListPeoples() { Clear(); }
+
+    void Print()
+    {
+        Element* temp = First;
+        while (temp != nullptr)
+        {
+            temp->PData.Print();
+            temp = temp->next;
+        }
+    }
+    int Count() { return SizeList; }
+
+    void Insert(PeoplDt peopleData, int index)
+    {
+        if (index < 0 || index == 0) InsertFront(peopleData);
+        else
+        {
+            if (index > SizeList || index == SizeList) InsertBack(peopleData);
+            else
+            {
+                Element* temp = First;
+                int count = 0;
+                while (temp != nullptr)
+                {
+                    if (count == index - 1)
+                    {
+                        Element* people = new Element(peopleData);
+                        people->next = temp->next;
+                        people->pre = temp;
+                        temp->next = people;
+                        people->next->pre = people;
+                        SizeList++;
+                        break;
+                    }
+                    count++;
+                    temp = temp->next;
+                }
+            }
+        }
+    }
+    void Delete(int index)
+    {
+        if (index < 0 || index == 0) DeleteFront();
+        else
+        {
+            if (index > SizeList || index == SizeList - 1) DeleteBack();
+            else
+            {
+                Element* temp = First;
+                int count = 0;
+                while (temp != nullptr)
+                {
+                    if (count == index - 1)
+                    {
+                        Element* toDelete = temp->next;
+                        temp->next = toDelete->next;
+                        temp->next->pre = temp;
+                        delete toDelete;
+                        SizeList--;
+                        break;
+
+                    }
+                    count++;
+                    temp = temp->next;
+                }
+
+            }
+        }
+    }
+    Element Get(int index)
+    {
+        if (index == SizeList - 1) { return Last->PData; }
+        else
+        {
+            if (index == 0) return First->PData;
+            else
+            {
+                Element* temp = First;
+                int count = 0;
+                while (temp != nullptr)
+                {
+                    if (count == index) { return temp->PData; }
+                    count++;
+                    temp = temp->next;
+                }
+            }
+        }
+    }
+    bool Set(string Name, string Surname, int Age, int index)
+    {
+        if (index == SizeList - 1)
+        {
+            Last->PData.Name = Name;
+            Last->PData.Surname = Surname;
+            Last->PData.Age = Age;
+            return true;
+        }
+        else
+        {
+            if (index == 0)
+            {
+                First->PData.Name = Name;
+                First->PData.Surname = Surname;
+                First->PData.Age = Age;
+                return true;
+            }
+            else
+            {
+                Element* temp = First;
+                int count = 0;
+                while (temp != nullptr)
+                {
+                    if (count == index)
+                    {
+                        temp->PData.Name = Name;
+                        temp->PData.Surname = Surname;
+                        temp->PData.Age = Age;
+                        return true;
+                    }
+                    count++;
+                    temp = temp->next;
+                }
+            }
+        }
+        return false;
+    }
+    void Clear()
+    {
+        while (SizeList) DeleteFront();
+    }
+
+private:
+    Element* First;
+    Element* Last;
+    int SizeList;
+    void InsertFront(PeoplDt peopleData)
+    {
+        if (First == nullptr)
+        {
+            First = Last = new Element(peopleData);
+        }
+        else
+        {
+            Element* people = new Element(peopleData);
+            people->next = First;
+            First->pre = people;
+            First = people;
+        }
+        SizeList++;
+    }
+    void InsertBack(PeoplDt peopleData)
+    {
+        if (First == nullptr)
+        {
+            First = Last = new Element(peopleData);
+        }
+        else
+        {
+            Element* people = new Element(peopleData);
+            Last->next = people;
+            people->pre = Last;
+            Last = people;
+        }
+        SizeList++;
+    }
+    void DeleteFront()
+    {
+        Element* temp = First;
+        First = First->next;
+        if (SizeList > 1)
+            First->pre = nullptr;
+        SizeList--;
+        delete temp;
+    }
+    void DeleteBack()
+    {
+        if (First != nullptr)
+        {
+            Element* temp = Last;
+            Last = Last->pre;
+            Last->next = nullptr;
+            SizeList--;
+            delete temp;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    ListPeoples ppl;
+    ppl.Insert(PeoplDt("Никита", "Ковалев ", 18), 0);
+    ppl.Insert(PeoplDt("Адольф", "Ефимович ", 15), 1);
+    ppl.Insert(PeoplDt("Стёпан", "Мышьев ", 18), 2);
+    ppl.Insert(PeoplDt("Иван", "Пушкин ", 33), 3);
+    bool b = ppl.Set("Дмитрий", "Александров", 23, 3);
+    cout << "\nЗамена данных: " << b << endl;
+    Element people;
+    people = ppl.Get(2);
+    cout << "\nЗначение элемента:\n ";
+    people.PData.Print();
+    cout << "\nУдаление элемента:\n";
+    ppl.Delete(2);
+    ppl.Print();
+    cout << "\nКоличество элементов: " << ppl.Count() << endl;
+    ppl.Clear();
+    cout << "\nКоличество элементов: " << ppl.Count() << endl;
+    return 0;
+}
---
+++ Универсальная коллекция/GenericСollection/GenericСollection.cpp
@@ -0,0 +1,209 @@
+﻿#include <iostream>
+using namespace std;
+template<typename T>
+class List
+{
+private:
+    template<typename T>
+    class Node
+    {
+    public:
+        Node* pNext;
+        Node* pPrev;
+        T data;
+
+        Node(T data = T(), Node* pNext = nullptr, Node* pPrev = nullptr)
+        {
+            this->data = data;
+            this->pNext = pNext;
+            this->pPrev = pPrev;
+        }
+    };
+    int SizeList;
+    Node<T>* First;
+    Node<T>* Last;
+public:
+    List()
+    {
+        SizeList = 0;
+        First = Last = nullptr;
+    }
+    ~List()
+    {
+        Clear();
+    }
+    int Count() { return SizeList; }
+    void Insert(T data, int index)
+    {
+        if (index <= 0) InsertFront(data);
+        else
+        {
+            if (index >= SizeList) InsertBack(data);
+            else
+            {
+                Node<T>* temp = First;
+                int count = 0;
+                while (temp != nullptr)
+                {
+                    if (count == index - 1)
+                    {
+                        Node<T>* element = new Node<T>(data);
+                        element->pNext = temp->pNext;
+                        element->pPrev = temp;
+                        temp->pNext = element;
+                        element->pNext->pPrev = element;
+                        SizeList++;
+                        break;
+                    }
+                    count++;
+                    temp = temp->pNext;
+                }
+            }
+        }
+
+
+
+
+    }
+    void InsertFront(T data)
+    {
+        if (First == nullptr)
+        {
+            First = Last = new Node<T>(data);
+        }
+        else
+        {
+            Node<T>* element = new Node<T>(data);
+            element->pNext = First;
+            First->pPrev = element;
+            First = element;
+        }
+        SizeList++;
+    }
+    void InsertBack(T data)
+    {
+        if (First == nullptr)
+        {
+            First = Last = new Node<T>(data);
+        }
+        else
+        {
+            Node<T>* element = new Node<T>(data);
+            Last->pNext = element;
+            element->pPrev = Last;
+            Last = element;
+        }
+        SizeList++;
+    }
+    void Delete(int index)
+    {
+        if (index <= 0) DeleteFront();
+        else
+        {
+            if (index > SizeList || index == SizeList - 1) DeleteBack();
+            else
+            {
+                Node<T>* temp = First;
+                int count = 0;
+                while (temp != nullptr)
+                {
+                    if (count == index - 1)
+                    {
+                        Node<T>* toDelete = temp->pNext;
+                        temp->pNext = toDelete->pNext;
+                        temp->pNext->pPrev = temp;
+                        delete toDelete;
+                        SizeList--;
+                        break;
+
+                    }
+                    count++;
+                    temp = temp->pNext;
+                }
+
+            }
+        }
+    }
+    void DeleteFront()
+    {
+        if (First != nullptr)
+        {
+            Node<T>* temp = First;
+            First = First->pNext;
+            if (SizeList > 1)
+                First->pPrev = nullptr;
+            SizeList--;
+            delete temp;
+        }
+    }
+    void DeleteBack()
+    {
+        if (First != nullptr)
+        {
+            Node<T>* temp = Last;
+            Last = Last->pPrev;
+            Last->pNext = nullptr;
+            SizeList--;
+            delete temp;
+        }
+    }
+    T& operator[](const int index)
+    {
+        int count = 0;
+
+        Node<T>* temp = First;
+
+        while (temp != nullptr)
+        {
+            if (count == index)
+            {
+                return temp->data;
+            }
+            temp = temp->pNext;
+            count++;
+        }
+    }
+    void Clear()
+    {
+        while (SizeList) DeleteFront();
+    }
+
+};
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    List<int> list;
+    list.InsertBack(5);
+    list.InsertFront(1);
+    list.InsertFront(6666);
+    list.Insert(666, 1);
+    list[2] = 5;
+    for (int i = 0; i < list.Count(); i++)
+    {
+        cout << list[i] << endl;
+    }
+    cout << "" << endl;
+    cout << "Удаление элемента " << endl;
+    list.Delete(2);
+    for (int i = 0; i < list.Count(); i++)
+    {
+        cout << list[i] << endl;
+    }
+    cout << "" << endl;
+    List<string>lst;
+    lst.InsertBack("Элент №2");
+    lst.InsertFront("Элент №344");
+    lst.InsertFront("Элент №244");
+    for (int i = 0; i < lst.Count(); i++)
+    {
+        cout << lst[i] << endl;
+    }
+    cout << "" << endl;
+    cout << "Удаление элемента" << endl;
+    lst.DeleteFront();
+    for (int i = 0; i < lst.Count(); i++)
+    {
+        cout << lst[i] << endl;
+    }
+    return 0;
+}
