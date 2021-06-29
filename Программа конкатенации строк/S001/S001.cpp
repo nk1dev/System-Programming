fb0b30f26ec95450538bd90469b7478ea1028783
nk1dev <76609654+nk1dev@users.noreply.github.com>
2021-06-29 10:52:23 +0300

    up

---
+++ Программа конкатенации строк/S001/S001.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+int main()   //Программа конкатенации строк
+
+{
+    std::cout << "Vedite 2 stroki\n";
+    char masif[514];
+    char str[256]; 
+    char str2[256]; 
+    char f[] = "%s";
+    
+    __asm
+    {
+
+        lea esi, str
+        push esi
+        lea ecx, f
+        push ecx
+        call scanf
+        add esp, 8
+        mov ecx, -1
+        dec esi
+
+        lea esi, str2
+        push esi
+        lea ecx, f
+        push ecx
+        call scanf
+        add esp, 8
+        mov ecx, -1
+        dec esi
+
+        lea esi, [str]
+        lea edi, [masif]
+        b1:
+        lodsb
+            stosb
+            cmp al, 0
+            jnz b1
+
+            lea esi, [str2]
+            dec edi
+
+            b2 :
+        lodsb
+            stosb
+            cmp al, 0
+            jnz b2
+
+    }
+    std::cout << masif;
+}
