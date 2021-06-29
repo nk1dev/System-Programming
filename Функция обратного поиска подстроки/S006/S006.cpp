459268147b87c718078cb26976b8c4f137c08c68
nk1dev <76609654+nk1dev@users.noreply.github.com>
2021-06-29 11:18:53 +0300

    up

---
+++ Функция обратного поиска подстроки/S006/S006.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+
+int main() //Функция обратного поиска подстроки
+{
+	char str[255];
+	char FindString[255];
+	char f[] = "%s";
+	char fd[] = "%d\n";
+	char outStr1[256] = "Enter main string: ";
+	char outStr2[256] = "Enter find string: ";
+	char outPut[256] = "Result: ";
+	__asm {
+
+		lea edx, outStr1
+		push edx
+		lea ecx, f
+		push ecx
+		call printf
+		add esp, 8
+
+		lea eax, str
+		push eax
+		lea ecx, f
+		push ecx
+		call scanf
+		add esp, 8
+
+		lea edx, outStr2
+		push edx
+		lea ecx, f
+		push ecx
+		call printf
+		add esp, 8
+
+		lea eax, FindString
+		push eax
+		lea ecx, f
+		push ecx
+		call scanf
+		add esp, 8
+
+		lea edx, outPut
+		push edx
+		lea ecx, f
+		push ecx
+		call printf
+		add esp, 8
+
+		lea eax, str
+		dec eax
+		mov ecx, 0
+		dec ecx
+
+		CountLen :
+		inc eax
+			inc ecx
+			cmp[eax], 0
+			jnz CountLen
+
+			lea ebx, FindString
+			dec ebx
+			mov edx, 0
+			dec edx
+
+			CountLenSub :
+		inc ebx
+			inc edx
+			cmp[ebx], 0
+			jnz CountLenSub
+
+
+			push edx
+			cmp edx, ecx
+			jg NoMatch
+			cmp edx, 0
+			jz NoMatch
+
+			dec eax
+			dec ebx
+
+			Cycle :
+		cmp edx, 0
+			jz OutPutResult
+			cmp ecx, 0
+			jz NoMatch
+			push ecx
+			mov ch, [eax]
+			mov cl, [ebx]
+			cmp ch, cl
+			pop ecx
+			jz skip
+			pop edx
+			lea ebx, FindString
+			add ebx, edx
+			push edx
+			dec ebx
+			push ecx
+			mov ch, [eax]
+			mov cl, [ebx]
+			cmp ch, cl
+			pop ecx
+			jz skip
+			dec eax
+			dec ecx
+			jmp Cycle
+			skip :
+		dec eax
+			dec ebx
+			dec ecx
+			dec edx
+			jmp Cycle
+
+			NoMatch :
+		mov ecx, -1
+			OutPutResult :
+			push ecx
+			lea ecx, fd
+			push ecx
+			call printf
+			add esp, 12
+
+	}
+}
---
+++ Функция поиска подстроки/S005/S005.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+
+int main()// Функция поиска подстроки
+{
+    char str[256];
+    char findSTR[256];
+  
+    char f[] = "%s";
+    char fd[] = "%d";
+
+    std::cout << "String:";
+    char poiskSTR[256] = "Poisk string:";
+
+    __asm
+    {
+        lea ebx, str
+
+        push ebx
+
+        lea ecx, f
+
+        push ecx
+
+        call scanf
+
+        add esp, 8
+
+        lea edx, poiskSTR
+
+        push edx
+
+        lea ecx, f
+
+        push ecx
+
+        call printf
+
+        add esp, 8
+
+        lea edx, findSTR
+
+        push edx
+
+        lea ecx, f
+
+        push ecx
+
+        call scanf
+
+        add esp, 8
+
+        lea ebx, str
+        lea edx, findSTR
+        dec ebx
+        dec edx
+        lea ecx, fd
+        mov ecx, -1
+        push ecx
+
+        FindStr :
+        inc ebx
+            inc ecx
+            mov al, [ebx]
+            cmp al, 0
+            jz RepeatComp
+            jmp Continue
+            RepeatComp :
+        inc edx
+            mov ah, [edx]
+            cmp ah, 0
+            jz end
+            add esp, 4
+            mov eax, -1
+            push eax
+            jmp end
+            Continue :
+        inc edx
+            mov ah, [edx]
+            cmp ah, 0
+            jz end
+            cmp al, ah
+            jz CompareResult
+            lea edx, findSTR
+            dec edx
+            add esp, 4
+            mov eax, -1
+            push eax
+            jmp FindStr
+            CompareResult :
+        pop eax
+            cmp eax, -1
+            jg Next
+            push ecx
+            jmp FindStr
+            Next :
+        push eax
+            jmp FindStr
+
+            end :
+        lea ebx, fd
+            push ebx
+            call printf
+            add esp, 8
+    }
+}
+
